c7325e2d85ab0cc8e9ca35b84059b0cc58af1a57
DodgyMerchant <malte.boenig@gmail.com>
2024-11-12 10:30:56 +0100

    Added some game data get and set. Aswell as overlap check.

--- main.gd
+++ main.gd
@@ -56,8 +56,6 @@ func _show(target: CanvasItem):
 
 func _on_loadRequest(loadType: Globals.LOAD_TYPE, path: String):
 
-	var loadedData: Dictionary
-
 	match loadType:
 		Globals.LOAD_TYPE.GameDat_Json:
 			
@@ -74,18 +72,61 @@ func _on_loadRequest(loadType: Globals.LOAD_TYPE, path: String):
 			pass
 
 #endregion
+#region data processing
 
-## data processing
+## given raw game data will return the gun name at index.
+func gamedata_get_entry(rawData: Variant, key: String) -> String:
+	return rawData.data[key].basic.name
+
+## given raw game data will set the gun name at key index. returns if successful.
+func gamedata_set_entry(rawData: Variant, key: String, newData: String) -> bool:
+
+	if !rawData.data.has(key):
+		return false
+	
+	rawData.data[key].basic.name = newData
+
+	return true
+
+## converts raw game data to a dictionary.
 func gamedata_to_dict(rawData: Variant) -> Dictionary:
-	
-	rawData = rawData.data
 
-	var data: Dictionary = {}
+	var dict: Dictionary = {}
 
-	for i in rawData.keys():
-		data[i] = rawData[i].basic.name
+	for i in rawData.data.keys():
+		dict[i] = gamedata_get_entry(rawData, i)
 	
-	return data
+	return dict
+
+## given raw game data and a dictionary containing gun data names and display names, will apply the display names to corresponding entries in the game data.
+func dict_applyTo_gamedata(rawData: Variant, dict: Dictionary) -> Array[String]:
+
+	## list of items that exist in the dictionary but not the game data.
+	var notFoundList: Array[String] = []
+
+	for i in dict.keys():
+		if !gamedata_set_entry(rawData, i, dict[i]):
+			notFoundList.append(i)
+	
+	return notFoundList
 
 func _on_gameData_updated(gameData):
 	determine_screen()
+
+#endregion
+
+## determines if all items from dict1 exist in dict2. Returns a list of keys missing from dict 2. 
+func _dict_overlap(dict1: Dictionary, dict2: Dictionary) -> Array[String]:
+
+	if dict2.has_all(dict1.keys()):
+		return []
+	
+	var list: Array[String] = []
+
+	for i in dict1.keys():
+		if dict2.has(i):
+			continue
+		
+		list.append(i)
+
+	return list
